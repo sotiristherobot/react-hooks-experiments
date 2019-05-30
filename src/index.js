b2236ae80f9a5d4934d6b05cc58f2b8b42f67da6
Sotiris Ioannou <sotiris@jsmate.net>
2019-05-30 19:24:58 +0200

    Installed Prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
- 
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
