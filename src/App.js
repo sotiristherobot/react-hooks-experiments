6f4abd0313835d8dd8078aa4a593065f0c49d8a8
Sotiris Ioannou <sotiris@jsmate.net>
2019-06-13 11:07:30 +0200

    Added lazy loading for routes
    Since some of the routes we don't know whether will be visited,
    lazy loading was introduced. The routes are only loaded on request.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 // components
 import Home from "./components/home/Home";
 
+// lazy load routes
+const Profile = React.lazy(() => import("./components/profile/Profile"));
+
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={Home} />
-    </Router>
+    <Suspense fallback={<div>Loading</div>}>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/profile" component={Profile} />
+      </Router>
+    </Suspense>
   );
 }
 
