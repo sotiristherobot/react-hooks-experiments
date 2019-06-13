6d09f949448c606ba8be4883deb060b8ca6b40fa
Sotiris Ioannou <sotiris@jsmate.net>
2019-06-13 16:29:50 +0200

    Tmp css for profile form

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { Box, Form, FormField, Button } from "grommet";
+import "./Profile.css";
 
 export default function Profile() {
   return (
     <Box
-      align="center"
-      alignContent="between"
-      justify="around"
-      alignSelf="stretch"
+        direction="row"
+        align="center"
+        justify="around"
+        alignSelf="stretch"
     >
-      <Form>
-        <FormField name="name" label="Name" />
-        <FormField name="username" label="Username" />
-        <Button type="submit" primary label="Submit" />
+      <Form className="profileForm">
+        <FormField name="name" label="Name" required />
+        <FormField name="username" label="Username" required />
+        <FormField name="email" label="Email" />
+        <FormField name="phone" label="Phone" />
+        <Button type="submit" primary label="Submit" fill={false} />
       </Form>
     </Box>
   );
