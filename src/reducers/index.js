8731f25e52a649fdeac49a176c7439eab66e0acb
Sotiris Ioannou <sotiris@jsmate.net>
2019-06-21 13:50:46 +0200

    Added articles.reducer to redux reducers file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import user from './user.reducer';
+import articles from './articles.reducer';
 
-const rootReducer = combineReducers({user});
+const rootReducer = combineReducers({user, articles});
 export default rootReducer;
