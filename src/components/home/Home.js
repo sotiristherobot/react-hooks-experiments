8fcd36cd1ba13ec7710c2a81f4d18fb6642ee949
Sotiris Ioannou <sotiris@jsmate.net>
2019-06-24 13:34:44 +0200

    Added infinite scrolling to articles in the main component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Box } from "grommet";
+import { Box, InfiniteScroll } from "grommet";
 import { useDispatch, useSelector } from "react-redux";
 import { API_REQUEST_ARTICLES } from "../../constants/actionTypes";
 import Article from "../article/Article";
@@ -13,19 +13,21 @@ export default function Home() {
   }, []);
 
   return (
-      <Box>
-        <Box
-            width="60%"
-            direction="column"
-            alignSelf="center"
-        >
-          {
-            !articles.loadingArticles && articles.map(article =>
-                <Article key={article.id} title={article.title} content={article.content} image={article.image} />
-            )
-          }
-        </Box>
+    <Box>
+      <Box width="60%" direction="column" alignSelf="center">
+        {!articles.loadingArticles && (
+          <InfiniteScroll items={articles}>
+            {article => (
+              <Article
+                key={article.id}
+                title={article.title}
+                content={article.content}
+                image={article.image}
+              />
+            )}
+          </InfiniteScroll>
+        )}
       </Box>
-
+    </Box>
   );
 }
