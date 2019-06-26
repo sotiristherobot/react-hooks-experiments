5b912d7c04b8713628ef7552a86c8c626c0d497e
Sotiris Ioannou <sotiris@jsmate.net>
2019-06-26 14:24:52 +0200

    Color like icon based on click and last state

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "grommet";
 import { Like, Dislike } from "grommet-icons";
 
@@ -6,11 +6,12 @@ import styled from "styled-components";
 
 export default function Article(props) {
   const StatusBar = styled.div`
-    color: red;
     width: 100%;
     margin-top: auto;
     align-self: flex-start;
   `;
+  const [isLiked, setLiked] = useState(false);
+  const onLikeClick = () => (isLiked ? setLiked(false) : setLiked(true));
 
   return (
     <Box
@@ -22,23 +23,14 @@ export default function Article(props) {
       width="medium"
       overflow="hidden"
     >
-      <Box
-        direction="column"
-        pad="small"
-      >
-      <h1>{props.title}</h1>
-      <p>{props.content}</p>
+      <Box direction="column" pad="small">
+        <h1>{props.title}</h1>
+        <p>{props.content}</p>
       </Box>
       <StatusBar>
-        <Box
-            direction="row"
-            pad="small"
-            gap="medium"
-            justify="stretch"
-            background="red"
-        >
-          <Like />
-          <Dislike/>
+        <Box direction="row" pad="small" gap="medium" justify="stretch">
+          <Like color={isLiked ? "blue" : null} onClick={onLikeClick} />
+          <Dislike />
         </Box>
       </StatusBar>
     </Box>
