fa3910d4d8e0f045739b99cb163ea58ccbf9ce02
Sotiris Ioannou <sotiris@jsmate.net>
2019-07-05 09:30:54 +0200

    Added edit mode for saving the form
    <Profile/> is now stateful, having an isEditMode flag. If set to true
    then the form is enabled, and if it's set to false the form is disabled
    On edit mode a save button appears which is hidden otherwise.

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Form, FormField, Button } from "grommet";
-import { Edit } from "grommet-icons";
+import { Edit, Save } from "grommet-icons";
 
 export default function Profile() {
+  const [isEditMode, setIsEditMode] = useState(false),
+    editButtonClicked = function() {
+      isEditMode ? setIsEditMode(false) : setIsEditMode(true);
+    };
+
   return (
     <Box align="center">
-      <Box width="60%" pad={{top: 'medium'}}>
-        <Box direction="row" justify="end">
-          <Button icon={<Edit/>} fill="vertical" label="Edit" onClick={() => {}} />
+      <Box width="60%" pad={{ top: "medium" }}>
+        <Box direction="row" justify="end" gap="small">
+          {isEditMode ? (
+            <Button
+              icon={<Save />}
+              fill="vertical"
+              label="Save"
+              onClick={() => {
+                alert("saved");
+                setIsEditMode(false);
+              }}
+            />
+          ) : null}
+          <Button
+            icon={<Edit />}
+            fill="vertical"
+            label="Edit"
+            onClick={editButtonClicked}
+          />
         </Box>
         <Form>
-          <FormField name="name" label="Name" required />
-          <FormField name="username" label="Username" required />
-          <FormField name="email" label="Email" />
-          <FormField name="phone" label="Phone" />
+          <FormField name="name" label="Name" required disabled={!isEditMode} />
+          <FormField
+            name="username"
+            label="Username"
+            required
+            disabled={!isEditMode}
+          />
+          <FormField name="email" label="Email" disabled={!isEditMode} />
+          <FormField name="phone" label="Phone" disabled={!isEditMode} />
           <Button
             type="submit"
             primary
             label="Submit"
             fill={false}
+            disabled={!isEditMode}
           />
         </Form>
       </Box>
