07eb2ffa260c12b4e1c6b76d3be851e84f5c3dc8
Sotiris Ioannou <sotiris@jsmate.net>
2019-07-19 11:15:20 +0200

    Added initial <DataTable/>

--- src/components/article/likedarticles/LikedArticlesList.js
+++ src/components/article/likedarticles/LikedArticlesList.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { Box, DataTable } from "grommet";
+import { Like } from "grommet-icons/es6";
 
-function LikedArticlesList () {
-    return 'Liked Articles Page';
+function LikedArticlesList() {
+  return (
+    <Box align="center">
+      <Box width="60%" align="stretch">
+        <DataTable
+          alignSelf="center"
+          columns={[
+            {
+              property: "id",
+              header: "id",
+              primary: true
+            },
+            {
+              property: "title",
+              header: "title"
+            },
+            {
+              property: "statusbar",
+              header: "status",
+              render: status => (
+                <Box>
+                  <Like size="medium" />
+                </Box>
+              )
+            }
+          ]}
+          data={[
+            { id: 0, title: "testr", percent: 20 },
+            { id: 1, title: "test1", percent: 30 },
+            { id: 2, title: "test2", percent: 40 },
+            { id: 3, title: "test3", percent: 80 }
+          ]}
+        />
+      </Box>
+    </Box>
+  );
 }
-
 export default LikedArticlesList;
