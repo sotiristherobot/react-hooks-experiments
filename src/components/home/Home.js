b6f9af0f7f18d68f583f6158b7ad1325a8c9d40b
Sotiris Ioannou <sotiris@jsmate.net>
2019-07-21 14:43:55 +0200

    Removed unused required from <Home/>

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Box, InfiniteScroll } from "grommet";
+import { Box } from "grommet";
 import { useDispatch, useSelector } from "react-redux";
 import { API_REQUEST_ARTICLES } from "../../constants/actionTypes";
 import Article from "../article/Article";
@@ -14,18 +14,16 @@ export default function Home() {
 
   return (
     <Box direction="row" wrap={true} justify="center">
-      {!articles.loadingArticles && (
-        articles.map(
-          (article, index) => (
-            <Article
-              key={article.id}
-              title={article.title}
-              content={article.content}
-              image={article.image}
-              articleIndex={index}
-            />
-          ))
-      )}
+      {!articles.loadingArticles &&
+        articles.map((article, index) => (
+          <Article
+            key={article.id}
+            title={article.title}
+            content={article.content}
+            image={article.image}
+            articleIndex={index}
+          />
+        ))}
     </Box>
   );
 }
