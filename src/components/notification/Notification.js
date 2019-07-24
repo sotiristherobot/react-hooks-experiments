139f208120fc3213e9f838f8b17ba5a51f73a35d
Sotiris Ioannou <sotiris@jsmate.net>
2019-07-24 15:15:10 +0200

    Added useEffect to <Notification/> to hide itself
    With this, after the specified time - which is passed as props - passes
    it sets show to false and resets texts. The setShowNotification is again
    passed as props.

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Box, Layer, Text } from "grommet";
 
 export default function Notification(props) {
+
+  useEffect(() => {
+    setTimeout(() => {
+      props.setShowNotification({ show: false, text: null });
+    }, props.timeLimit);
+  });
+
   return (
     <Layer
       modal={false}
