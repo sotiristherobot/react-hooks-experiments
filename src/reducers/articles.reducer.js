0772f7d85bbb00f537142468a25200e13c36ab94
Sotiris Ioannou <sotiris@jsmate.net>
2019-07-24 12:22:51 +0200

    Articles reducer cleanup
    Removed redundant spread state and loading articles variable.

--- src/reducers/articles.reducer.js
+++ src/reducers/articles.reducer.js
@@ -13,15 +13,11 @@ export default function articlesReducer(
       return { ...state, loadingArticles: true, articles: [] };
     case API_REQUEST_ARTICLES_SUCCESS:
       return {
-        ...state,
-        loadingArticles: false,
         articles: action.payload.articles,
         error: null
       };
     case API_REQUEST_ARTICLES_FAILURE:
       return {
-        ...state,
-        loadingArticles: false,
         articles: [],
         error: action.payload.error
       };
