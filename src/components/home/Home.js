8b574299e19c257fdf80ebdf680b5fc8bc28b5d7
Sotiris Ioannou <sotiris@jsmate.net>
2019-07-24 12:25:51 +0200

    Added isLoading state to control the display and hide of articles
    Notify user if articles are still loading. If we don't do that, the user
    sees an ugly effect while waiting for the HTTP call to complete.

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Box } from "grommet";
 import { useDispatch, useSelector } from "react-redux";
 import { API_REQUEST_ARTICLES } from "../../constants/actionTypes";
@@ -6,15 +6,23 @@ import Article from "../article/Article";
 
 export default function Home() {
   const articles = useSelector(state => state.articles.articles),
-    dispatch = useDispatch();
+    dispatch = useDispatch(),
+    [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     dispatch({ type: API_REQUEST_ARTICLES });
   }, []);
 
+  useEffect(
+    () => (articles.length > 0 ? setIsLoading(false) : setIsLoading(true)),
+    [articles]
+  );
+
   return (
     <Box direction="row" wrap={true} justify="center">
-      {!articles.loadingArticles &&
+      {isLoading ? (
+        <p>Loading articles...</p>
+      ) : (
         articles.map((article, index) => (
           <Article
             key={article.id}
@@ -23,7 +31,8 @@ export default function Home() {
             image={article.image}
             articleIndex={index}
           />
-        ))}
+        ))
+      )}
     </Box>
   );
 }
