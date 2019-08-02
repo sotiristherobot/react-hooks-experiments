fb3ceb4477742dbe402ba049c33c89af55b0a269
Sotiris Ioannou <sotiris@jsmate.net>
2019-08-02 15:44:58 +0200

    Cleanup function for setting the notification only if the component has not unmounted
    This solves a bug when the user was navigating to another page while
    the notification was shown. To solve this we take advantage of closures and
    we set state only if the component is there.

--- src/components/notification/Notification.js
+++ src/components/notification/Notification.js
@@ -2,11 +2,21 @@ import React, { useEffect } from "react";
 import { Box, Layer, Text } from "grommet";
 
 export default function Notification(props) {
-
   useEffect(() => {
+    /*
+     This solves a bug when the user was navigating to another page while
+     the notification was shown. To solve this we take advantage of closures and
+     we set state only if the component is there.
+    */
+    let isNotificationActive = true;
     setTimeout(() => {
-      props.setShowNotification({ show: false, text: null });
+      isNotificationActive &&
+        props.setShowNotification({ show: false, text: null });
     }, props.timeLimit);
+
+    return function cleanup() {
+      isNotificationActive = false;
+    };
   });
 
   return (
