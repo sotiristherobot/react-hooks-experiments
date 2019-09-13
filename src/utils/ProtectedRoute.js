6e356c1e1a9f23e296b19a705a5787bb1b3e5b2b
Sotiris Ioannou <sotiris@jsmate.net>
2019-09-13 11:38:33 +0200

    If not authed <ProtectedRoute/> redirects to '/login'

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
-//ToDo Login logic should be implemented here.
-const isAuthed = () => false;
-
-export default function ProtectedRoute({ component: Component, ...rest }) {
-  return isAuthed() ? (
+export default function ProtectedRoute({ component: Component, isAuthed,  ...rest }) {
+  return isAuthed? (
     <Route {...rest} render={props => <Component />} />
   ) : (
-    <p>Not authorized to view this route</p>
+    <Redirect to={{pathname: "/login"}}/>
   );
 }
