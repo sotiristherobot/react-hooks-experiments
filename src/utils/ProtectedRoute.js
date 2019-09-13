30ff39188e459356ea7f959875934c9c2db4eb51
Sotiris Ioannou <sotiris@jsmate.net>
2019-09-13 11:13:28 +0200

    Added basic authorization to <PrivateRoute/>
    Changed the component to include checking for authorization

--- src/utils/ProtectedRoute.js
+++ src/utils/ProtectedRoute.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
-export default function ProtectedRoute({ component:Component, ...rest }) {
-  return <Route {...rest} render={props => <Component/>} />;
+//ToDo Login logic should be implemented here.
+const isAuthed = () => false;
+
+export default function ProtectedRoute({ component: Component, ...rest }) {
+  return isAuthed() ? (
+    <Route {...rest} render={props => <Component />} />
+  ) : (
+    <p>Not authorized to view this route</p>
+  );
 }
