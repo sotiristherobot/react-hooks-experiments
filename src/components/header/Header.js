7a6d9401ae2138b21d8e9154488149019d6ecaf4
Sotiris Ioannou <sotiris@jsmate.net>
2019-09-13 11:37:37 +0200

    If the user is not authorized then <Header/> is hidden

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,30 +1,36 @@
-import React  from "react";
+import React from "react";
 import { Box, Heading } from "grommet";
 import Avatar from "../avatar/Avatar";
-import { withRouter } from 'react-router-dom';
+import { withRouter } from "react-router-dom";
 
 const HeaderStyle = {
   backgroundColor: "#243959"
 };
 
-const Header = function(props) {
-  const { photo, name } = props.user,
-    onHeaderClick = () => props.history.push("/");
-  console.log('rerendering');
+const Header = function({user, history, isAuthed}) {
+  const { photo, name } = user,
+    onHeaderClick = () => history.push("/");
+  console.log("rerendering");
   return (
-    <Box align="center" background={HeaderStyle.backgroundColor} flex={{grow: 0, shrink: 0}}>
-      <Box width="80%" direction="row" justify="between" align="center">
-        <Heading level={3} margin="none" onClick={onHeaderClick}>
-          Sotiris
-        </Heading>
-        <Avatar url={photo} fullName={name} />
+    isAuthed && (
+      <Box
+        align="center"
+        background={HeaderStyle.backgroundColor}
+        flex={{ grow: 0, shrink: 0 }}
+      >
+        <Box width="80%" direction="row" justify="between" align="center">
+          <Heading level={3} margin="none" onClick={onHeaderClick}>
+            Sotiris
+          </Heading>
+          <Avatar url={photo} fullName={name} />
+        </Box>
       </Box>
-    </Box>
+    )
   );
 };
 /**
  * There is no need to re-render <Header/> on every navigation action.
  * <Header/> is now pure component using React.memo with it's own comparison function that always returns true on props comparisons,
  * to avoid re-renders based on the history, from withRouter HOC.
-*/
-export default withRouter(React.memo(Header, () => true ));
+ */
+export default withRouter(React.memo(Header, () => true));
