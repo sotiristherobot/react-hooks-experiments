d897aba6091299d24a301a0b95570e0ada64f7d1
Sotiris Ioannou <sotiris@jsmate.net>
2019-09-16 11:54:26 +0200

    Added <Form/> for login screen

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,4 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { Form, FormField, TextInput } from "grommet/es6";
 
-const Login = ({isAuthed}) => !isAuthed && <p>This is the login screen</p>;
+const Login = ({ isAuthed }) => {
+  const usernameInputRef = useRef(null);
+
+  useEffect(() => usernameInputRef.current.focus(),[usernameInputRef]);
+
+  return (
+    !isAuthed && (
+      <Form>
+        <FormField label="Username">
+          <TextInput placeholder="type username" ref={usernameInputRef} />
+        </FormField>
+          <FormField label="Password">
+              <TextInput type="password" placeholder="type password"/>
+          </FormField>
+      </Form>
+    )
+  );
+};
+
 export default React.memo(Login);
